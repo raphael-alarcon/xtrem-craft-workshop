946ad50251830b10c0167d1f2271c85ade55351b
Gaetan Berisset <berisset.gaetan@gmail.com>
2023-11-23 11:13:25 +0100

    commit

--- typescript/tests/Bank.spec.ts
+++ typescript/tests/Bank.spec.ts
@@ -2,6 +2,44 @@ import { Currency } from '../src/Currency'
 import { Bank } from '../src/Bank'
 import { MissingExchangeRateError } from '../src/MissingExchangeRateError'
 import { Money } from './Money.spec'
+import { map } from 'fp-ts/lib/Functor'
+
+class BankBuilder {
+  private pivotCurrency: Currency
+  private exchangeRates = []
+
+  add(currency: Currency, rate: number) {
+    this.exchangeRates.push({currency:currency, rate:rate})
+  }
+
+  static aBank(): BankBuilder {
+    return new BankBuilder; 
+  }
+
+  static aEuropeanBank = () => BankBuilder.aBank().withPivotCurrency(Currency.EUR); 
+
+  withPivotCurrency(currency: Currency): BankBuilder {
+    this.pivotCurrency = currency;
+    return this; 
+  }
+
+  withExchangeRate(currency: Currency, rate: number): BankBuilder {
+    this.exchangeRates.push(currency, rate);
+    return this; 
+  }
+
+  build(){
+    const bank = Bank.withExchangeRate(this.pivotCurrency, this.exchangeRates[0].currency, this.exchangeRates[0].rate);
+    this.exchangeRates.forEach(exchangeRates => {
+      bank.addExchangeRate(this.pivotCurrency, exchangeRates.currency, exchangeRates.rate);
+      bank.addExchangeRate(exchangeRates.currency, this.pivotCurrency, 1/exchangeRates.rate);
+    }); 
+    return Bank; 
+  }
+}
+
+
+
 
 describe('Bank', function () {
 
